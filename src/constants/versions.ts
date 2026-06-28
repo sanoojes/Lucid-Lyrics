f6dab1dfa717b008eb9384a43692115bbae12ccf
Sanooj Es <mail@sanooj.uk>
2026-06-28 22:27:37 +0530

    bump: spicy lyrics version

--- src/constants/versions.ts
+++ src/constants/versions.ts
@@ -1,3 +1,3 @@
 export const APP_NAME = __APP_NAME__; // will be added by the builder
 export const APP_VERSION = __APP_VERSION__; // will be added by the builder
-export const SPICY_APP_VERSION = "6.0.0";
+export const SPICY_APP_VERSION = "6.1.1";
